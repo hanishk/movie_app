49b0d4018eb6a38ff51c797433ca7c78b3174589
hanishk <hanishk616@gmail.com>
2025-08-30 21:38:02 +0530

    WIP: fake deep link using app links

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
+import 'package:app_links/app_links.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:movie_app/app/router.dart';
 import 'package:movie_app/app/service_locator.dart';
 
@@ -8,14 +12,67 @@ void main() async {
   runApp(const MovieApp());
 }
 
-class MovieApp extends StatelessWidget {
+class MovieApp extends StatefulWidget {
   const MovieApp({super.key});
+
+  @override
+  State<MovieApp> createState() => _MovieAppState();
+}
+
+class _MovieAppState extends State<MovieApp> {
+  late final GoRouter _router;
+  StreamSubscription<Uri>? _linkSub;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _router = AppRouter.router;
+
+    _initDeepLinks();
+  }
+
+  void _initDeepLinks() async {
+    final appLinks = AppLinks();
+
+    try {
+      // Cold start (app opened with a deep link)
+      final initialUri = await appLinks.getInitialLink();
+      if (initialUri != null) {
+        _handleDeepLink(initialUri);
+      }
+
+      // Hot links (when app is already running)
+      _linkSub = appLinks.uriLinkStream.listen((uri) {
+        if (uri != null) {
+          _handleDeepLink(uri);
+        }
+      });
+    } catch (e) {
+      debugPrint("Deep link error: $e");
+    }
+  }
+
+  void _handleDeepLink(Uri uri) {
+    if (uri.scheme == "movieapp" && uri.host == "movie") {
+      final id = uri.pathSegments.isNotEmpty ? uri.pathSegments.first : null;
+      if (id != null) {
+        _router.go("/movie/$id");
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    _linkSub?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
       title: 'Movie App',
-      routerConfig: AppRouter.router,
+      routerConfig: _router,
       debugShowCheckedModeBanner: false,
     );
   }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,11 +5,13 @@
 import FlutterMacOS
 import Foundation
 
+import app_links
 import path_provider_foundation
 import share_plus
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
